e7124bd42bdea58374861652743918f6bf7546e1
Mladen Čikara <mladen.cikara@gmail.com>
2020-07-02 20:50:22 +0200

    Adding bootstrap and Navigation bar to application

--- src/App.tsx
+++ src/App.tsx
@@ -74,18 +74,25 @@ function App() {
     return (
         <Router>
             <div>
-                <nav>
-                    <ul>
-                        <li>
-                            <Link to="/">Home</Link>
-                        </li>
-                        <li>
-                            <Link to="/about">About</Link>
-                        </li>
-                        <li>
-                            <Link to="/counter">Counter</Link>
-                        </li>
-                    </ul>
+                <nav className={"navbar navbar-expand-lg navbar-light bg-light"}>
+                    <Link className={"navbar-brand"} to="/">Home</Link>
+                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+                            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"/>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarNav">
+
+                        <ul className={"navbar-nav"}>
+                            <li className={"nav-item"}>
+                            </li>
+                            <li>
+                                <Link className={"nav-link"} to="/about">About</Link>
+                            </li>
+                            <li>
+                                <Link className={"nav-link"} to="/counter">Counter</Link>
+                            </li>
+                        </ul>
+                    </div>
                 </nav>
 
                 {/* A <Switch> looks through its children <Route>s and
