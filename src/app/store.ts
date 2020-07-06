1770b8a14b8788e6ee7da544c4a7388c65ad9752
Mladen Čikara <mladen.cikara@gmail.com>
2020-07-06 20:57:02 +0200

    Documentation

--- src/app/store.ts
+++ src/app/store.ts
@@ -9,7 +9,11 @@ export const store = configureStore({
   },
 });
 
+// TypeScript type is for that root state object
+// All we have to do is use the built-in TS ReturnType utility type, and feed in "the type of the rootReducer function"
+// as its generic argument.
 export type RootState = ReturnType<typeof store.getState>;
+
 export type AppThunk<ReturnType = void> = ThunkAction<
   ReturnType,
   RootState,
