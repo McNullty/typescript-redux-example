807542c99985a846793f619be56fe0a371ab5e3a
Mladen Čikara <mladen.cikara@gmail.com>
2020-07-07 20:47:06 +0200

    Saving store to local storage

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,13 +1,20 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
 import loginReducer from '../features/login/loginSlice';
+import {loadState, saveState} from "./localStorage";
+import throttle from 'lodash.throttle';
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
     login: loginReducer,
   },
+  preloadedState: loadState()
 });
+
+store.subscribe(throttle(() => {
+  saveState(store.getState());
+}, 1000));
 
 // TypeScript type is for that root state object
 // All we have to do is use the built-in TS ReturnType utility type, and feed in "the type of the rootReducer function"
